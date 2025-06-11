2ddd02c010ba4d1811d154e707d3f4da63200489
JorgeyArnau <erasmusroma20242025@gmail.com>
2025-06-11 12:50:44 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-// Inicializar animaciones on scroll (AOS)
-AOS.init({
-  once: true
-});
+AOS.init({ once:true });
 
+document.querySelectorAll('a[href^=\"#\"]').forEach(a=>{
+  a.addEventListener('click',e=>{
+    const t=document.querySelector(a.getAttribute('href'));
+    if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth'});}
+  });
+});
